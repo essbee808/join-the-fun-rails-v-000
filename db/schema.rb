9b0a362a854c0a24385790421887b415ef5786cd
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-10 04:54:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200310045132) do
+ActiveRecord::Schema.define(version: 20200310045345) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 20200310045132) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "taxi_id"
+    t.integer  "passenger_id"
   end
 
   create_table "taxis", force: :cascade do |t|
